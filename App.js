4528ae2ba34727b944e24a726f668abe3690f52d
Yousif <yousuf1212121@gmail.com>
2022-09-11 21:42:19 +0300

    task completed

---
+++ App.js
@@ -0,0 +1,11 @@
+//Welcome to my Nodejs repository :D
+
+//I well do simple greeting program just in case
+let date = new Date();
+let hour = date.getHours();
+
+if (hour < 12) {
+  console.log("Good morning everyone.");
+} else {
+  console.log("Good evening everyone.");
+}
